190679108f97ff22e9c868486ee6156d86098e44
Rose Sponseller <rosesponseller@gmail.com>
2017-06-30 16:21:46 -0700

    Clean up unnecessary comments and erroneous info

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,9 +1,5 @@
 var Doctor = require('./../js/scripts.js').doctorModule;
 
-// var name = function(name) {
-//   $('.result').before("<h3>Name: " + name + "</h3>");
-// };
-
 var doctorInfo = function(result) {
 result.data.forEach(function(doctor) {
     $('.result').append(
@@ -41,27 +37,21 @@ result.data.forEach(function(doctor) {
     "<li><h4>Zip code:</h4><li>" +
     doctor.practices[0].visit_address.zip +
     "</li></ul><hr>");
-
-    // Clear input for each submit
-    // $('.result').text("");
   });
 
 };
 
 $(function() {
-
   var newDoctor = new Doctor();
   var searchInput;
 
   $('.search-form').submit(function(e) {
     e.preventDefault();
     searchInput = $('.search-input').val();
-    // cityInput = $('.city-input').val();
 
     // Clear input for every submit
     $(".result").empty();
 
     newDoctor.getDoctor(searchInput, doctorInfo);
-
   });
 });
