3f6068f54d2f0d7d00ccc7661d2d5c5d874e046f
Rose Sponseller <rosesponseller@gmail.com>
2017-08-06 19:36:53 -0700

    Tweaks to work section styles, button and form stylings, and add smooth scrolling navigation

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -96,3 +96,20 @@ function animate () {
 
 animate();
 }
+
+/// Smooth navigation scrolling effect
+
+$(document).ready(function(){
+  $('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function() {
+  		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  			var target = $(this.hash);
+  			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  			if (target.length) {
+  				$('html,body').animate({
+  					scrollTop: target.offset().top
+  				}, 1000);
+  				return false;
+  			}
+  		}
+  	});
+});
